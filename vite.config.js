7f36ffad37a5c668ca5eae2c2e3b104626969c36
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2025-03-29 11:10:02 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 путь, с которого будет искаться index.html и ассеты
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

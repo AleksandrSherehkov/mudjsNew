4f20a9dd0680ae0e3ed38d1dbf7cddfb025128a5
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2025-03-29 18:00:58 +0200

    refactor: code

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import compression from 'vite-plugin-compression';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'brotliCompress', // або 'gzip'
+      ext: '.br',
+      threshold: 10240,
+    }),
+  ],
   base: '/',
   resolve: {
     alias: {
